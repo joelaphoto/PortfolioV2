2219a49ebb210599fa4c2874dbe223b767886cb3
Joel Adams <joeladamsdesign@gmail.com>
2018-04-25 13:07:06 -0700

    add project service to admin component, add admin link to navbar

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../services/project.service'
+import { Project } from '../models/project.model';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [ProjectService]
 })
+
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  projects: FirebaseListObservable<any[]>;
+
+  constructor(private projectService: ProjectService) {}
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
   }
 
 }
