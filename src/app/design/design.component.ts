fd204fa4a3de1ae279ff812bd3b3d115713e9a33
Joel Adams <joeladamsdesign@gmail.com>
2018-04-30 15:37:19 -0700

    add project list to design page

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../services/project.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+ 
 
 @Component({
   selector: 'app-design',
   templateUrl: './design.component.html',
-  styleUrls: ['./design.component.css']
+  styleUrls: ['./design.component.css'],
+  providers: [ProjectService]
 })
 export class DesignComponent implements OnInit {
+  projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private projectService: ProjectService, private router: Router) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
   }
 
 }
