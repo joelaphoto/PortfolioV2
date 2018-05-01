6d6e92aa2ea5808db27e46a1f219f2bcb6bf6fe0
Joel Adams <joeladamsdesign@gmail.com>
2018-05-01 15:44:22 -0700

    add user model, add login and logout methods, add login to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
+import { resolve } from 'path';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+
+  email: string;
+  password: string;
+  error: string;
 
   constructor(private authService: AuthenticationService, private router: Router) { }
 
-  ngOnInit() {
+  signIn() {
+    this.authService.login({email: this.email, password: this.password})
+    .then(resolve => this.router.navigate(['Admin']))
+    .catch(error => this.error = error.message);
   }
 
 }
