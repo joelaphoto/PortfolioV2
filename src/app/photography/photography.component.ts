5392bd2698f933ce53929cdbf0fe830d1c20223b
Joel Adams <joeladamsdesign@gmail.com>
2018-05-02 14:32:28 -0700

    debug category, switch to single file uploader, add category buttons

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnChanges } from '@angular/core';
 import { ImageService } from '../services/image.service';
 import { Observable } from 'rxjs/Observable';
 import { GalleryImage } from '../models/gallery-image.model';
+import { Upload } from '../models/upload.model';
 
 @Component({
   selector: 'app-photography',
@@ -10,6 +11,10 @@ import { GalleryImage } from '../models/gallery-image.model';
   providers: [ImageService]
 })
 export class PhotographyComponent implements OnInit, OnChanges {
+
+  showPortrait: boolean = false;
+  showEvent: boolean = false;
+  showProduct: boolean = false;
 
   images: Observable<GalleryImage[]>;
 
@@ -20,6 +25,38 @@ export class PhotographyComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
-    this.images = this.imageService.getImages(); 
+    this.images = this.imageService.getImages();
+  }
+
+  viewPortrait() {
+    this.showEvent = false;
+    this.showProduct = false;
+    this.showPortrait = true;
+  }
+
+  viewEvent() {
+    this.showEvent = true;
+    this.showProduct = false;
+    this.showPortrait = false;
+  }
+
+  viewProduct() {
+    this.showEvent = false;
+    this.showProduct = true;
+    this.showPortrait = false;
+  }
+
+
+
+  display(image) {
+    if (this.showPortrait == true && image.category == 'portrait') {
+      return true;
+    } else if (this.showEvent == true && image.category == 'event') {
+      return true;
+    } else if (this.showProduct == true && image.category == 'product') {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -19,11 +19,12 @@ export class UploadComponent {
   handleFiles(event){
     this.files = event.target.files
   }
-  uploadFiles(title: string, description: string){
+  uploadFiles(title: string, description: string, category: string){
     const filesToUpload = this.files;
       this.upload = new Upload(filesToUpload[0]);
       this.upload.title = title;
       this.upload.description = description;
+      this.upload.category = category;
       this.uploadService.uploadFiles(this.upload);
   }
 
