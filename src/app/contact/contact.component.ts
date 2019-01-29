d574fe3e7819a6a8675db6c89642018dc305078b
Joel Adams <joeladamsdesign@gmail.com>
2019-01-29 14:31:57 -0800

    add contact component logic

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
@@ -12,4 +13,34 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
+  sendMessage(email: string, message: string) {
+    if (email === '') {
+      return alert('Please Enter an Email');
+    } else if (message === '') {
+      return alert('Please Enter a Message')
+    } else {
+      var destinationEmail = "joeladamsphotography@gmail.com";
+      var form = document.createElement('form');
+      form.setAttribute("action", "https://formspree.io/" + destinationEmail)
+      form.setAttribute("method", "POST")
+
+      // Subject for your email
+      var field = document.createElement("input");
+      field.setAttribute("type", "hidden");
+      field.setAttribute("name", "message");
+      field.setAttribute("value", message);
+      form.appendChild(field);
+
+      // Contact email address        
+      field = document.createElement("input");
+      field.setAttribute("type", "hidden");
+      field.setAttribute("name", "email");
+      field.setAttribute("value", email);
+      form.appendChild(field);
+
+      document.body.appendChild(form);
+      form.submit();
+    }
+  }
 }
+
