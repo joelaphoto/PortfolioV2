284ad9be14b6bc8cbcb2f904b11a6fa7238f4c17
Joel Adams <joeladamsdesign@gmail.com>
2019-01-29 14:22:17 -0800

    add contact html, routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { HomepageComponent } from './homepage/homepage.component'
 import { DevelopmentComponent } from './development/development.component'
 import { PhotographyComponent } from './photography/photography.component';
@@ -24,6 +25,10 @@ const appRoutes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: 'login',
