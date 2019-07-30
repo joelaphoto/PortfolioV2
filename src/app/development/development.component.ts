d2849ebe9d3bb85a5f1e539c126714f1b3b4f765
Joel Adams <joeladamsdesign@gmail.com>
2019-07-29 19:27:03 -0700

    update development component
    Add new skill logos, write method to caulclate number of months in each skill to reduce necessity for constant updates.

--- src/app/development/development.component.ts
+++ src/app/development/development.component.ts
@@ -19,6 +19,29 @@ export class DevelopmentComponent implements OnInit {
     this.projects = this.projectService.getProjects();
   }
 
+  calulateMonths(dateString) {
+    const d1 = new Date(dateString);
+    const d2 = new Date();
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    if (months < 2) {
+      return months + " Month";
+    } else if (months < 12) {
+      return months + " Months";
+    } else if (months >= 12) {
+      const dispMonths = months % 12;
+      const years = (months - dispMonths) / 12;
+      if (years < 2) {
+        return years + " Year " + months + " Months";
+      } else {
+        return years + " Years " + months + " Months";
+      }
+    }
+    // return months <= 0 ? 0 : months + " Months";
+  }
+
   changeDate(dateString) {
     const year = dateString.slice(0,4)
     const month = dateString.slice(5,7)
