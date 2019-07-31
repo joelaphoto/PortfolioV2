dad96b0295a0e4210c26fc4aea3e4dad65273efa
Joel Adams <joeladamsdesign@gmail.com>
2019-07-31 14:34:57 -0700

    add year calculator to homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -18,4 +18,29 @@ export class HomepageComponent implements OnInit {
     el.scrollIntoView({behavior: "smooth"});
   }
 
+  calulateYears(dateString) {
+    const d1 = new Date(dateString);
+    const d2 = new Date();
+    let months;
+
+    function isYears(years) {
+      if (years < 2) {
+        return " Year"
+      } else {
+        return " Years"
+      }
+    }
+
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    if (months < 12) {
+      return "Less Than 1 Year"
+    } else if (months >= 12) {
+      const dispMonths = months % 12;
+      const years = (months - dispMonths) / 12;
+      return years + isYears(years);
+    }
+  }
+
 }
