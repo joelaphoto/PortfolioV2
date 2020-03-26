8091f66ba4788fe026c4bfebb4ed37d705b649bd
Joel Adams <joeladamsdesign@gmail.com>
2020-03-25 17:08:29 -0700

    add animations on scroll

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as jquery from 'jquery';
 
 @Component({
   selector: 'app-homepage',
@@ -19,6 +20,34 @@ export class HomepageComponent implements OnInit {
 
 
   ngOnInit() {
+    var $animation_elements = jquery('.row');
+    var $window = jquery(window);
+    $window.on('scroll', check_if_in_view);
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+
+
+    function check_if_in_view() {
+      var window_height = $window.height();
+      var window_top_position = $window.scrollTop();
+      var window_bottom_position = (window_top_position + window_height);
+    
+      jquery.each($animation_elements, function() {
+        var $element = jquery(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+    
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+          $element.addClass('in-view');
+        } 
+        // else {
+        //   $element.removeClass('in-view');
+        // }
+      });
+    }
   }
 
   scroll() {
