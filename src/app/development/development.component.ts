3eeab837b83f896a92e05b786f16dd2b7811bebc
Joel Adams <joeladamsdesign@gmail.com>
2020-03-26 14:15:57 -0700

    add loading spinner to dev component

--- src/app/development/development.component.ts
+++ src/app/development/development.component.ts
@@ -27,30 +27,7 @@ export class DevelopmentComponent implements OnInit {
     this.projectService.getProjects().subscribe(data => {
       this.projects = data;
       console.log(this.projects.length)
-      var $animation_elements = jquery('.row');
-      var $window = jquery(window);
-      $window.on('scroll', check_if_in_view);
-      $window.on('scroll resize', check_if_in_view);
-      $window.trigger('scroll');
 
-      function check_if_in_view() {
-        var window_height = $window.height();
-        var window_top_position = $window.scrollTop();
-        var window_bottom_position = (window_top_position + window_height);
-      
-        jquery.each($animation_elements, function() {
-          var $element = jquery(this);
-          var element_height = $element.outerHeight();
-          var element_top_position = $element.offset().top;
-          var element_bottom_position = (element_top_position + element_height);
-      
-          //check to see if this current container is within viewport
-          if ((element_bottom_position >= window_top_position) &&
-              (element_top_position <= window_bottom_position)) {
-            $element.addClass('in-view');
-          } 
-        });
-      }
     });;
     this.user = this.authService.authUser();
 
@@ -109,7 +86,30 @@ export class DevelopmentComponent implements OnInit {
       this.imagesLoadedPercentage = 100;
       jquery('div.loader').fadeOut(800, function() {
         jquery('div.container').css('opacity', '1');
-
+        var $animation_elements = jquery('div.project.row');
+        var $window = jquery(window);
+        $window.on('scroll', check_if_in_view);
+        $window.on('scroll resize', check_if_in_view);
+        $window.trigger('scroll');
+  
+        function check_if_in_view() {
+          var window_height = $window.height();
+          var window_top_position = $window.scrollTop();
+          var window_bottom_position = (window_top_position + window_height);
+        
+          jquery.each($animation_elements, function() {
+            var $element = jquery(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+        
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+              $element.addClass('in-view');
+            } 
+          });
+        }
       });
     } else {
       this.imagesLoaded = this.imagesLoaded + 1;
